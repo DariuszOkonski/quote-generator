9a9c7d1bb9bc57076db7494194ce0ec98bb0f0ab
Dariusz Okonski <darek200180@gmail.com>
2020-09-10 12:42:21 +0100

    add loader

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ async function getQuote() {
     // const proxyUrl = 'https://afternoon-hollows-80162.herokuapp.com/';
     // const apiUrl = 'http://api.forismatic.com/api/1.0/?method-getQuote&lang=en&format=json';
     const apiUrl = 'https://api.icndb.com/jokes/random';
-
+    
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
