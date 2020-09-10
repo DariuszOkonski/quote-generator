f13d9303bfbbe226dd5503fb697383ac9d3a4782
Dariusz Okonski <darek200180@gmail.com>
2020-09-10 12:05:59 +0100

    use jokes api instead of forismatic due to problems with cors at forismatic

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('Hello world 1');
+// http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en
+// http://forismatic.com/en/api
+
+// get quote from api
+
+async function getQuote() {
+    // const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    // const proxyUrl = 'https://afternoon-hollows-80162.herokuapp.com/';
+    // const apiUrl = 'http://api.forismatic.com/api/1.0/?method-getQuote&lang=en&format=json';
+    const apiUrl = 'https://api.icndb.com/jokes/random';
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        console.log(data);
+        console.log(data.value.id);
+        console.log(data.value.joke);
+    } catch (error) {
+        console.log('Whoops, no quote: ', error);
+    }
+
+}
+
+// on load
+getQuote();
